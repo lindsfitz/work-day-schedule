c42207a424666f3902614c71bd7150122dd21ea9
lindsfitz <lindsaytfitz@gmail.com>
2021-09-30 10:25:44 -0700

    fix js error

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 // todays date
 var savedItems = { };
 
+// Sets the current date and time via moment
 var today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY HH:mm"))
 
 var updatedTime = setInterval(function() {
     $("#date-time").text(today);
 }, 1000);
+
+
+// Function compares the current hour (via moment.js) to the time assigned to each time block in the HTML to assign a bgcolor based on past, present, or future
+$("textarea").each(function () {
+    var name = parseInt($(this).attr("name"));
+    var todayCheck = moment().format("H")
+    if (name < todayCheck) {
+        $(this).addClass("past");
+    }
+    
+    if (name > todayCheck) {
+        $(this).addClass("future")
+    }
+    
+    if (name === todayCheck) {
+        $(this).addClass("present")
+    }
+})
 
 
 var retrieveItems = JSON.parse(localStorage.getItem("savedItems"));
@@ -25,22 +44,8 @@ function init () {
     })
 }
 
-$("textarea").each(function () {
-    var name = parseInt($(this).attr("name"));
-    var todayCheck = moment().format("H")
-    if (name < todayCheck) {
-        $(this).addClass("past");
-    }
 
-    if (name > todayCheck) {
-        $(this).addClass("future")
-    }
-
-    if (name === todayCheck) {
-        $(this).addClass("present")
-    }
-})
-
+// function called when the save button is clicked. Saves the text input of the specific element into a local storage object with the key set to the hour of the particular element 
 function saveItem() {
     var textElement = $(this).siblings('textarea')
     var newTask = textElement.val()
@@ -50,7 +55,7 @@ function saveItem() {
 }
 
 
-
+//when you click the save button within each time block, it will call the saveItem function 
 $(".saveBtn").each(function(){
     $(this).click(saveItem)
 })
